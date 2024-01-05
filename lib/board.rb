ac63e84872ecdfdb9901e83ef7adcc203a1d1984
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-05 16:14:52 -0700

    Feat: adjust #display_board, #create_board, add #count_columns to Board class

--- lib/board.rb
+++ lib/board.rb
@@ -3,21 +3,30 @@ class Board
         create_board
     end
 
-    # init @board as an array containing seven instances of Column class
+    # create a board as an array containing seven instances of Column class
     def create_board
         @board = Array.new(7) {Column.new}
-        @board[0].column_label = "A"
-        @board[1].column_label = "B"
-        @board[2].column_label = "C"
-        @board[3].column_label = "D"
-        @board[4].column_label = "E"
-        @board[5].column_label = "F"
-        @board[6].column_label = "G"
+        @board[0].column_letter = "A"
+        @board[1].column_letter = "B"
+        @board[2].column_letter = "C"
+        @board[3].column_letter = "D"
+        @board[4].column_letter = "E"
+        @board[5].column_letter = "F"
+        @board[6].column_letter = "G"
         display_board
     end
 
+    # puts column letters, iterates through board to map individual rows, puts rows
     def display_board
         puts "A B C D E F G"
-        @board.each {|row| puts "#{row[0..6].join} \n"}
+        (0..5).each do |row|
+            current_row = @board.map {|column| column.cells[row].token || column.cells[row].cell}.join("\n")
+            puts current_row
+        end
+    end
+
+    # verify correct count of columns; can be deleted if unnecessary, used for testing purposes
+    def count_columns
+        @board.size
     end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,4 +8,8 @@ RSpec.describe Board do
     it "creates a board object" do        
         expect(@board).to be_an_instance_of(Board)
     end
+
+    it "#count_columns" do
+        expect(@board.count_columns).to eq 7
+    end
 end
