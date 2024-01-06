a8bc61299618581b0cdfe0d2bed8d6f191d67c48
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-06 12:33:37 -0700

    Feat: add line to convert player input to column number

--- lib/game.rb
+++ lib/game.rb
@@ -66,13 +66,14 @@ class Game
     end
 
     def column_full?(current_player_input)
-        #make current player input change to be 0-6
+        column_number = current_player_input.ord - 'A'.ord #takes A-G and subtracts ASCII value (65) from the player input, so 65-65 = 0 which is A, 65-64 = 1 which is B, etc.
         columned_board = @board.board.transpose
         if current_player_input == "A"
             columned_board[0]
         end
         @board = columned_board.transpose
     end
+
 
 =begin
 board[row][column]
