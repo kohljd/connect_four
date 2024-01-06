e7c35d620535f8d164a1ce679b14a248190ea81e
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-06 13:57:35 -0700

    Feat: add win? methods

--- lib/game.rb
+++ lib/game.rb
@@ -103,20 +103,35 @@ board[0][3] - "A"
             #take_turn
     end
 
-    ## Mel's Notes:
+    ## Notes:
     # Methods to consider:
     # Win conditions: Diagonal (Top left to bottom right & top right to bottom left), Horiz, Vert
     # Tie condition: Gameboard full
 
     def tie_game?
         @board.board.each do |row|
-            row.each do |cell|
-                if cell != "."
-                    true
-                end
-            end
+            return false if row.any? {|cell| cell == "."}
         end
+        true
     end
+
+    def vertical_win?
+        transposed_board = @board.board.transpose
+        transposed_board.each do |row|
+            return false if row.any? {|cell| cell == "."}
+        end
+        true
+    end
+
+    def horizontal_win?
+        @board.board.each do |row|
+            return false if row.any? {|cell| cell == "."}
+        end
+        true
+    end
+
+
+
 
 
 
