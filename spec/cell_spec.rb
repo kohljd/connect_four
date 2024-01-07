13179050b2cd4656b3cd9533748e9a34553e9256
Jess Kohl <149989113+kohljd@users.noreply.github.com>
2024-01-07 16:25:19 -0600

    Test: starting cell value

--- spec/cell_spec.rb
+++ spec/cell_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe Cell do
         expect(@cell).to be_an_instance_of Cell
     end
 
+    it "starts with \'.\'" do
+        expect(@cell.cell_value).to eq(".")
+    end
+
     it "#empty?" do
         expect(@cell.cell_empty?).to be true
         @cell.cell_value = "X"
