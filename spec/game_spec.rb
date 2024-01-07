12ab1b881065e91dd3a926ef6b3b57b694ac76f1
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-07 10:38:11 -0700

    Test: attempting to fix this mess

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,240 +1,259 @@
 require "./spec/spec_helper"
 
 RSpec.describe Game do
-    before(:each) do
-        @game = Game.new
-        @board_1 = [
-            ['X', '.', '.', '.', '.', '.', '.'],
-            ['.', 'X', '.', '.', '.', '.', '.'],
-            ['.', '.', 'X', '.', '.', '.', '.'],
-            ['.', '.', '.', 'X', '.', '.', '.'],
-            ['.', '.', '.', '.', '.', '.', '.'],
-            ['.', '.', '.', '.', '.', '.', '.'],
-            ['.', '.', '.', '.', '.', '.', '.']
-        ]
+    describe "#game_menu" do
+      it "starts the game when user inputs 'play'" do
+        allow_any_instance_of(Object).to receive(:gets).and_return("play\n")
+  
+        game = Game.new
+        expect(game).to receive(:start_game)
+  
+        game.game_menu
+      end
     end
+  end
 
-    it "starts the game when user inputs 'play'" do
-        allow(@game).to receive(:gets).and_return("sdkfjkdj\n")
-        expect(@game.game_menu).to include("Invalid input:")
-        # @game.game_menu("play\n")
+# RSpec.describe Game do
+#     before(:each) do
+#         @game = Game.new
+#         @board_1 = [
+#             ['X', '.', '.', '.', '.', '.', '.'],
+#             ['.', 'X', '.', '.', '.', '.', '.'],
+#             ['.', '.', 'X', '.', '.', '.', '.'],
+#             ['.', '.', '.', 'X', '.', '.', '.'],
+#             ['.', '.', '.', '.', '.', '.', '.'],
+#             ['.', '.', '.', '.', '.', '.', '.'],
+#             ['.', '.', '.', '.', '.', '.', '.']
+#         ]
+#     end
 
-        expect(@game).to receive(:start_game)
-    end
+#     it "creates a game object" do
+#         expect(@game).to be_an_instance_of(Game)
+#     end
 
-    it "creates a game object" do
-        expect(@game).to be_an_instance_of(Game)
-    end
+#     it "starts the game when user inputs 'play'" do
+#         allow_any_instance_of(Object).to receive(:gets).and_return("play\n")
+  
+#         game = Game.new
+#         expect(game).to receive(:start_game)
+  
+#         game.game_menu
+#         # allow(@game).to receive(:gets).and_return("sdkfjkdj\n")
+#         # expect(@game.game_menu).to include("Invalid input:")
+#         # # @game.game_menu("play\n")
 
-    describe "beginning" do
-        it "game menu accepts user input" do
-            # checks to see that "play" will call #start_game
-            expect(@game).to receive(:start_game)
-        end
-    end
+#         # expect(@game).to receive(:start_game)
+#     end
 
-    describe "turn" do
-        before :each do
-            @current_player_input = "A"
-            @player_1 = @game.player_1
-            @player_2 = @game.player_2
-        end
+#     describe "beginning" do
+#         it "game menu accepts user input" do
+#             allow(@game).to receive(:gets).and_return("play\n")
+#             expect(@game).to receive(:start_game)
+#             @game.game_menu
+#         end
+#     end
 
-        it "alternates between player and computer" do
-            expect(@game.current_player).to eq(@player_1)
+#     describe "turn" do
+#         before :each do
+#             @current_player_input = "A"
+#             @player_1 = @game.player_1
+#             @player_2 = @game.player_2
+#         end
 
-            @game.change_player
+#         it "alternates between player and computer" do
+#             expect(@game.current_player).to eq(@player_1)
 
-            expect(@game.current_player).to eq(@player_2)
-        end
+#             @game.change_player
 
-        it "updates the board" do
-            # starting with a board of empty cells
-            updated_board = [
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.']
-            ]
+#             expect(@game.current_player).to eq(@player_2)
+#         end
 
-            expect(place_token(@current_player_input)).to eq(updated_board)
-        end
-    end
+#         it "updates the board" do
+#             # starting with a board of empty cells
+#             updated_board = [
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.']
+#             ]
 
-    describe "for player" do
-        describe "validates turn input" do
-            it "#valid_column?" do
-                # setup of #column_full?, may not need the next line
-                allow(@game).to receive(:column_full?)
-                @game.valid_column?("A")
+#             expect(place_token(@current_player_input)).to eq(updated_board)
+#         end
+#     end
 
-                expect(@game.valid_column?("A")).to have_received(:column_full?)
+#     describe "for player" do
+#         describe "validates turn input" do
+#             it "#valid_column?" do
+#                 # setup of #column_full?, may not need the next line
+#                 allow(@game).to receive(:column_full?)
+#                 @game.valid_column?("A")
 
-                @game.valid_column?("H")
-                expect(@game.valid_column?("H")).to output(/Invalid column name:/).to_stdout
-            end
+#                 expect(@game.valid_column?("A")).to have_received(:column_full?)
 
-            describe "#column_full?" do
-                before :each do
-                    @full_board = [
-                        ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
-                        ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
-                        ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
-                        ['O', 'X', 'X', 'O', 'O', 'X', 'X'],
-                        ['O', 'O', 'X', 'X', 'X', 'O', 'O'],
-                        ['O', 'O', 'X', 'O', 'X', 'O', 'O'],
-                        ['X', 'X', 'O', 'X', 'O', 'O', 'X']
-                    ]
-                    @full_board_game = Game.new
-                    @game.instance_variable_set(:@board, @board_1)
-                    @full_board_game.instance_variable_set(:@board, @full_board)
-                end
+#                 @game.valid_column?("H")
+#                 expect(@game.valid_column?("H")).to output(/Invalid column name:/).to_stdout
+#             end
 
-                it "if the column is not full" do
-                    expect(@game.column_full?("A")).to have_received(:place_token)
+#             describe "#column_full?" do
+#                 before :each do
+#                     @full_board = [
+#                         ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
+#                         ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
+#                         ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
+#                         ['O', 'X', 'X', 'O', 'O', 'X', 'X'],
+#                         ['O', 'O', 'X', 'X', 'X', 'O', 'O'],
+#                         ['O', 'O', 'X', 'O', 'X', 'O', 'O'],
+#                         ['X', 'X', 'O', 'X', 'O', 'O', 'X']
+#                     ]
+#                     @full_board_game = Game.new
+#                     @game.instance_variable_set(:@board, @board_1)
+#                     @full_board_game.instance_variable_set(:@board, @full_board)
+#                 end
 
-                    # valid entry
-                    expect(@game.valid_column?).to be true
-                end
+#                 it "if the column is not full" do
+#                     expect(@game.column_full?("A")).to have_received(:place_token)
 
-                it "if the column is full" do
-                    # invalid entry with an error message displayed
-                    expect(@game.valid_column?).to be false
-                end
-            end
-        end
-    end
+#                     # valid entry
+#                     expect(@game.valid_column?).to be true
+#                 end
 
-    describe "for computer" do
-        it "plays a token in a random column" do
-            # take_turn
-        end
+#                 it "if the column is full" do
+#                     # invalid entry with an error message displayed
+#                     expect(@game.valid_column?).to be false
+#                 end
+#             end
+#         end
+#     end
 
-        describe "checks input validity" do
-            it "if a valid column option" do
-                # valid entry
-                expect(@game.valid_move?).to be true
+#     describe "for computer" do
+#         it "plays a token in a random column" do
+#             # take_turn
+#         end
 
-                # invalid entry with an error message displayed
-                expect(@game.valid_move?).to be false
-            end
+#         describe "checks input validity" do
+#             it "if a valid column option" do
+#                 # valid entry
+#                 expect(@game.valid_move?).to be true
 
-            it "if the column is full" do
-                # valid entry
-                expect(@game.valid_move?).to be true
+#                 # invalid entry with an error message displayed
+#                 expect(@game.valid_move?).to be false
+#             end
 
-                # invalid entry with an error message displayed
-                expect(@game.valid_move?).to be false
-            end
-        end
-    end
+#             it "if the column is full" do
+#                 # valid entry
+#                 expect(@game.valid_move?).to be true
 
-    describe "ending" do
-        it "checks vertical win" do
-            winning_board = [
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.']
-            ]
-            losing_game = Game.new
-            @game.instance_variable_set(:@board, winning_board)
-            losing_game.instance_variable_set(:@board, @board_1)
+#                 # invalid entry with an error message displayed
+#                 expect(@game.valid_move?).to be false
+#             end
+#         end
+#     end
 
-            expect(@game.vertical_win?).to be true
-            expect(@game.vertical_win?).to be false
-        end
+#     describe "ending" do
+#         it "checks vertical win" do
+#             winning_board = [
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.']
+#             ]
+#             losing_game = Game.new
+#             @game.instance_variable_set(:@board, winning_board)
+#             losing_game.instance_variable_set(:@board, @board_1)
 
-        it "checks for horizontal win" do
-            winning_board = [
-                ['X', 'X', 'X', 'X', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],               
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.']
-            ]
-            losing_game = Game.new
-            @game.instance_variable_set(:@board, winning_board)
-            losing_game.instance_variable_set(:@board, @board_1)
+#             expect(@game.vertical_win?).to be true
+#             expect(@game.vertical_win?).to be false
+#         end
 
-            expect(@game.horizontal_win?).to be true
-            expect(losing_game.horizontal_win?).to be false
-        end
+#         it "checks for horizontal win" do
+#             winning_board = [
+#                 ['X', 'X', 'X', 'X', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],               
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.']
+#             ]
+#             losing_game = Game.new
+#             @game.instance_variable_set(:@board, winning_board)
+#             losing_game.instance_variable_set(:@board, @board_1)
 
-        it "checks for diagonal win" do
-            winning_board = [
-                ['.', '.', '.', '.', '.', '.', 'X'],
-                ['.', '.', '.', '.', '.', 'X', '.'],
-                ['.', '.', '.', '.', 'X', '.', '.'],
-                ['.', '.', '.', 'X', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.']
-            ]
-            losing_board = [
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['X', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.'],
-                ['.', '.', '.', '.', '.', '.', '.']
-            ]
-            game_1 = Game.new
-            losing_game = Game.new
-            @game.instance_variable_set(:@board, @board_1)
-            game_1.instance_variable_set(:@board, winning_board)
-            losing_game.instance_variable_set(:@board, losing_board)
+#             expect(@game.horizontal_win?).to be true
+#             expect(losing_game.horizontal_win?).to be false
+#         end
 
-            # top-left to bottom-right
-            expect(@game.diagonal_win?).to be true
-            # top-right to bottom-left
-            expect(game_1.diagonal_win?).to be true
-            expect(losing_game.diagonal_win?).to be false
-        end
+#         it "checks for diagonal win" do
+#             winning_board = [
+#                 ['.', '.', '.', '.', '.', '.', 'X'],
+#                 ['.', '.', '.', '.', '.', 'X', '.'],
+#                 ['.', '.', '.', '.', 'X', '.', '.'],
+#                 ['.', '.', '.', 'X', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.']
+#             ]
+#             losing_board = [
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['X', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.'],
+#                 ['.', '.', '.', '.', '.', '.', '.']
+#             ]
+#             game_1 = Game.new
+#             losing_game = Game.new
+#             @game.instance_variable_set(:@board, @board_1)
+#             game_1.instance_variable_set(:@board, winning_board)
+#             losing_game.instance_variable_set(:@board, losing_board)
 
-        it "checks for a tie" do
-            tie_board = [
-                ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
-                ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
-                ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
-                ['O', 'X', 'X', 'O', 'O', 'X', 'X'],
-                ['O', 'O', 'X', 'X', 'X', 'O', 'O'],
-                ['O', 'O', 'X', 'O', 'X', 'O', 'O'],
-                ['X', 'X', 'O', 'X', 'O', 'O', 'X']
-            ]
-            not_a_tie_game = Game.new
-            @game.instance_variable_set(:@board, tie_board)
-            not_a_tie_game.instance_variable_set(:@board, @board_1)
+#             # top-left to bottom-right
+#             expect(@game.diagonal_win?).to be true
+#             # top-right to bottom-left
+#             expect(game_1.diagonal_win?).to be true
+#             expect(losing_game.diagonal_win?).to be false
+#         end
 
-            expect(@game.tie_game?).to be true
-            expect(not_a_tie_game.tie_game?).to be false
-        end
+#         it "checks for a tie" do
+#             tie_board = [
+#                 ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
+#                 ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
+#                 ['X', 'O', 'O', 'X', 'X', 'O', 'O'],
+#                 ['O', 'X', 'X', 'O', 'O', 'X', 'X'],
+#                 ['O', 'O', 'X', 'X', 'X', 'O', 'O'],
+#                 ['O', 'O', 'X', 'O', 'X', 'O', 'O'],
+#                 ['X', 'X', 'O', 'X', 'O', 'O', 'X']
+#             ]
+#             not_a_tie_game = Game.new
+#             @game.instance_variable_set(:@board, tie_board)
+#             not_a_tie_game.instance_variable_set(:@board, @board_1)
 
-        it "checks for wins with consecutive tokens" do
-            # code, use @board_1
-        end
+#             expect(@game.tie_game?).to be true
+#             expect(not_a_tie_game.tie_game?).to be false
+#         end
 
-        it "displays the end game message" do
-            # code
-        end
+#         it "checks for wins with consecutive tokens" do
+#             # code, use @board_1
+#         end
 
-        it "does not allow additional turns" do
-            # end_game/#play_again combo or delete this one
-        end
+#         it "displays the end game message" do
+#             # code
+#         end
 
-        it "returns the user to the game menu" do
-            # play_again
-        end
-    end
-end
+#         it "does not allow additional turns" do
+#             # end_game/#play_again combo or delete this one
+#         end
 
+#         it "returns the user to the game menu" do
+#             # play_again
+#         end
+#     end
+# end
